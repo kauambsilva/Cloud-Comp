be03f62de1811e8391c72c18b05af2ae49db4b65
Kaua Marques <kauambsilva@fac.pe.senac.br>
2022-12-16 19:26:49 -0300

    commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const server = express();
+
+server.get("/ping", (req, res) => {
+  return res.json({ ping: "pong" });
+});
+
+server.use(express.json());
+
+server.listen(process.env.PORT || 3000, () => {
+  console.log("Servidor online");
+});
